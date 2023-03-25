faac290d87ed88bca91b549c7753c3307dd38a44
Breanna Nesbit <brebrenesbit02@gmail.com>
2023-03-24 21:49:29 -0600

    first ratio

--- TDDPractice/Deer.cs
+++ TDDPractice/Deer.cs
@@ -11,6 +11,13 @@ namespace TDDPractice
         public double TotalDeerCount;
         public double BuckTotal;
         public double DoeTotal;
+        public double FawnTotal;
+        public int[] PrimeNum = new [] {2,3,5,7,13};
+
+        public void AddFawnCount(double count)
+        {
+            FawnTotal += count; 
+        }
 
         public void AddingNewGeneration()
         {
@@ -28,6 +35,45 @@ namespace TDDPractice
         public void AddToDoeTotal(double newDoeCount)
         {
             DoeTotal += newDoeCount;
+        }
+
+        public string DoeToFawnRatio(double doe, double fawn)
+        {
+            double simplifiedDoe = doe;
+            double simplifiedFawn = fawn;
+            string simplifiedString = "";
+            foreach(var i in PrimeNum)
+            {
+                if(doe%i == 0  && fawn%i == 0)
+                {
+                    simplifiedString = DoeToFawnRatio(doe/i, fawn/i);
+                }
+            }
+
+            if(simplifiedString == "")
+            {
+                return $"{simplifiedDoe}:{simplifiedFawn}";
+            }
+            else
+                return simplifiedString;
+            
+            /*if(DoeTotal > FawnTotal)
+            {
+                if(DoeTotal%FawnTotal != 0)
+                {
+                    return $"{DoeTotal}:{FawnTotal}";
+                }
+                return $"{DoeTotal/FawnTotal}:{FawnTotal/FawnTotal}";
+            }
+
+            else if(DoeTotal < FawnTotal)
+            {
+                if(FawnTotal%DoeTotal != 0)
+                {
+                    return $"{DoeTotal}:{FawnTotal}";
+                }
+            }*/
+            
         }
 
         public void IncrementDeerTotal()
--- TDDTest/UnitTest1.cs
+++ TDDTest/UnitTest1.cs
@@ -49,5 +49,97 @@ namespace TDDTest
             Assert.AreEqual(16, deer.BuckTotal);
             Assert.AreEqual(42, deer.TotalDeerCount);
         }
+
+        [Test]
+        public void FawnCount()
+        {
+            deer.AddFawnCount(0);
+            Assert.AreEqual(0, deer.FawnTotal);
+            deer.AddFawnCount(5);
+            Assert.AreEqual(5, deer.FawnTotal);
+            deer.AddFawnCount(10);
+            Assert.AreEqual(15, deer.FawnTotal);
+        }
+
+        [Test]
+        public void DoeToDawnRatio()
+        {
+            deer.AddFawnCount(1);
+            deer.AddToDoeTotal(1);
+            var ratio = deer.DoeToFawnRatio(deer.DoeTotal, deer.FawnTotal);
+            Assert.AreEqual("1:1",ratio);
+        }
+
+        [Test]
+        public void DoeToDawnRatio2()
+        {
+            deer.AddFawnCount(2);
+            deer.AddToDoeTotal(2);
+            var ratio = deer.DoeToFawnRatio(deer.DoeTotal, deer.FawnTotal);
+            Assert.AreEqual("1:1",ratio);
+        }
+
+        [Test]
+        public void DoeToDawnRatio3()
+        {
+            deer.AddFawnCount(3);
+            deer.AddToDoeTotal(3);
+            var ratio = deer.DoeToFawnRatio(deer.DoeTotal, deer.FawnTotal);
+            Assert.AreEqual("1:1",ratio);
+        }
+
+        [Test]
+        public void DoeToDawnRatio2_1()
+        {
+            deer.AddFawnCount(1);
+            deer.AddToDoeTotal(2);
+            var ratio = deer.DoeToFawnRatio(deer.DoeTotal, deer.FawnTotal);
+            Assert.AreEqual("2:1",ratio);
+        }
+
+        [Test]
+        public void DoeToDawnRatio3_2()
+        {
+            deer.AddFawnCount(2);
+            deer.AddToDoeTotal(3);
+            var ratio = deer.DoeToFawnRatio(deer.DoeTotal, deer.FawnTotal);
+            Assert.AreEqual("3:2",ratio);
+        }
+
+        [Test]
+        public void DoeToDawnRatio5_2()
+        {
+            deer.AddFawnCount(2);
+            deer.AddToDoeTotal(5);
+            var ratio = deer.DoeToFawnRatio(deer.DoeTotal, deer.FawnTotal);
+            Assert.AreEqual("5:2",ratio);
+        }
+
+        [Test]
+        public void DoeToDawnRatio21_7()
+        {
+            deer.AddFawnCount(7);
+            deer.AddToDoeTotal(21);
+            var ratio = deer.DoeToFawnRatio(deer.DoeTotal, deer.FawnTotal);
+            Assert.AreEqual("3:1",ratio);
+        }
+
+        [Test]
+        public void DoeToDawnRatio8_6()
+        {
+            deer.AddFawnCount(6);
+            deer.AddToDoeTotal(8);
+            var ratio = deer.DoeToFawnRatio(deer.DoeTotal, deer.FawnTotal);
+            Assert.AreEqual("4:3",ratio);
+        }
+
+        [Test]
+        public void DoeToDawnRatio81_9()
+        {
+            deer.AddFawnCount(9);
+            deer.AddToDoeTotal(81);
+            var ratio = deer.DoeToFawnRatio(deer.DoeTotal, deer.FawnTotal);
+            Assert.AreEqual("9:1",ratio);
+        }
     }
 }
